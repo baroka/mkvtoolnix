77797aa09f112cbe18a522b6cd05c46857aab594
baroka <carlos.bares@gmail.com>
2022-08-15 16:39:55 +0200

    commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,14 @@
 echo "mkvtoolnix start.."
 
 # Monitorize video folder
-inotifywait -m -r -e create,moved_to /home/video --includei "\.mkv$" --format %w%f | while read line; do
-  echo "change detected.. ${line}"
-  /home/scripts/init.sh "${line}"
+inotifywait -m -r -e create,moved_to /home/video --includei "\.mkv$|\/[^.]*$" --format %w%f | while read line; do  
+  if [ -d "${line}" ]
+  then
+    echo "directory. change detected.. ${line}"
+    sleep 3
+
+  else
+    echo "mkv. change detected.. ${line}"
+    /home/scripts/init.sh "${line}"
+  fi
 done
